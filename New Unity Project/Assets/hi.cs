0f3bbbebf1641f44cd88cf23b080b3e750697630
cephastsai <petertsai1994@gmail.com>
2015-11-30 23:53:36 +0800

    test

---
+++ New Unity Project/Assets/hi.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hi : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Holle World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
